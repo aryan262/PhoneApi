e28ed28cb7db6a2c7209afc4963120300b4596bb
Aryan Panchal <aryan.nikolas@gmail.com>
2024-11-20 18:20:39 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import countriesData from './countries.json' assert { type: 'json' };
+import cors from 'cors';
 const app = express();
+app.use(cors('*'))
 const port = 5000;
 app.get("/", (req, res) => {
   res.json(countriesData);
